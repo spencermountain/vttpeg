68eea3cf9c3a65155051abe41c6d73287bfd5880
spencer kelly <spencerkelly86@gmail.com>
2026-01-11 14:38:09 -0500

    update same-diff

--- scratch.js
+++ scratch.js
@@ -3,23 +3,23 @@ import vttpeg from './src/main/index.js'
 
 let input = `WEBVTT
 
-00:22.908 --> 00:24.535
-Linda Johnson is
-a political liability.
+00:10.845 --> 00:17.845
+<i>-♪ The Mighty Boosh ♪
+-♪ Come with us to the Mighty Boosh ♪</i>
 
-00:24.618 --> 00:25.786
-Harry, I need her.
+00:23.965 --> 00:26.195
+[ELECTRONIC SQUELCHES, HUMMING]
 
-00:25.828 --> 00:26.953
-I can understand now
+00:34.204 --> 00:39.278
+-What the hell are you wearing?
+-This is the mirror ball suit.
 
-00:27.036 --> 00:28.287
-why Nelson
-finds you repugnant.`
+00:39.564 --> 00:40.554
+[SIGHING]
+`
 
 // const input = '/Volumes/4TB/subtitles/tv-shows/Columbo/S03/Columbo.S03E03.Candidate.For.Crime.vtt'
 // let txt = fs.readFileSync(input, 'utf8')
 let vtt = vttpeg(input)
-let html = vtt.diffCli()
-console.log(html)
+vtt.diffCli()
 // console.log(vtt.out())
